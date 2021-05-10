7709ba347e5e91fec171486b4d20b92f92e14517
Abdullah0505 <>
2021-05-10 19:47:14 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,20 @@
+// Variable temporarily stores user's inputted age and day of the week
+let age = 0
+let day = 0
+
+// Connect "Find Out" button to "findOut" function
+document.getElementById('button').addEventListener('click', findOut )
+
+function findOut () {
+  // Stores the user's age and converts it into an integer
+  age = document.getElementById('age').value
+  age = parseInt(age)
+  
+  day = document.getElementById('day').value
+  
+  // Program determines if user is eligable for student pricing
+  if ((day == "Tuesday" || day == "Thursday") || (age > 12 && age < 21))  {  document.getElementById('text').innerHTML =  'You\'re eligible for student pricing!'
+  } else {
+  document.getElementById('text').innerHTML = 'You must pay regular price'
+  }
+}
